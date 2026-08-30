76637e941a962fd54e795a2cce4b204add9d6b83
Jeffrey D Varner <jdv27@cornell.edu>
2026-08-30 12:02:18 -0400

    Release week-02.0: finalize student bundle

--- lectures/week-2/L2b/advanced/strips/Include.jl
+++ lectures/week-2/L2b/advanced/strips/Include.jl
@@ -1,5 +1,5 @@
 # setup paths -
-const _ROOT = pwd();
+const _ROOT = @__DIR__; # resolve local support files from the notebook folder
 const _PATH_TO_SRC = joinpath(_ROOT, "src");
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
 const _PATH_TO_FIGS = joinpath(_ROOT, "figs");
